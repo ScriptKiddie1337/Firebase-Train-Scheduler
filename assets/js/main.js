88b4a568b3b6791a5301f4102356203d958b3bfb
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-06-27 13:25:00 -0700

    framework and css

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD5FFf-lSm-qqOSHJSI2ZbgKiumbZFkJ9c",
+    authDomain: "train-scheduler-now.firebaseapp.com",
+    databaseURL: "https://train-scheduler-now.firebaseio.com",
+    projectId: "train-scheduler-now",
+    storageBucket: "",
+    messagingSenderId: "667512288625"
+  };
+  firebase.initializeApp(config);
