b4ccacc1aa05d403858005ef8c31421aca9f45b7
Script Kiddie <37413338+ScriptKiddie1337@users.noreply.github.com>
2018-07-19 17:50:54 -0700

    working version

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,29 +24,12 @@
     //gives local current time
   let currentTime = moment().format("hh:mm A");
   console.log("currentTime:", currentTime);
-    // To calculate the arrival time, add the tMinutes to the currrent time
-/*          let tFrequency = childchildSnapshot.val().frequency;
-         let tArrival = moment().add(tMinutes, "m").format("hh:mm A");
-         let tMinutes = tFrequency - tRemainder;
-         let tRemainder = moment().diff(moment.unix(tFirstTrain), "minutes") % tFrequency ;  */
-
          console.log(data);
          console.log(data.name);
 
-
-    /* if (childSnapshot.child("trainName").exists() && childSnapshot.child("destination").exists() && childSnapshot.child("frequency")) {
-
-      trainName = data.trainName;
-      destination = data.destination;
-      frequency = parseInt(data.frequency);
-      console.log(trainName);
-      console.log(destination);
-      console.log(frequency);
-    }
-   */
   trainName = data.name;
   destination = data.destination;
-  frequency = parseInt(data.frequency);
+  frequency = data.frequency;
   console.log(trainName);
   console.log(destination);
   console.log(frequency);
@@ -56,14 +39,17 @@
     $("#frequency").text(frequency); 
 
   //sets the new variable to a moment that subtracts (.diff()) the moment that the first train in minutes from the train frequency
-  let tRemainder = moment().diff(moment.unix(data.firstTrainUnix), "minutes") % frequency;
+  let tRemainder = moment().diff(moment.unix(data.firstTrain), "minutes") % frequency;
+  console.log(tRemainder);
+  console.log(data.firstTrainUnix);
   //now we subtract trainFrequency (ex. train comes every 30 minutes) from the tRemainder - how long is the remainder from when the first train arrived (ex. 10 min, giving us 20 mins until next train comes)
   let minutesAway = frequency - tRemainder;
 
   // To calculate the arrival time, add the tMinutes to the currrent time
   let nextArrival = moment().add(minutesAway, "m").format("hh:mm A");
 
-  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+  $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + parseInt(minutesAway) + "</td></tr>");
+  console.log(minutesAway);
     // If any errors are experienced, log them to console.
   }, function(errorObject) {
     console.log("The read failed: " + errorObject.code);
